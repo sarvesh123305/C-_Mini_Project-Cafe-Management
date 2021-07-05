aa9a5698323e696f68397cb24f42e401e8631ff7
Ashwin Kulkarni <ankulkarni731@gmail.com>
2021-07-05 17:24:25 +0530

    hii

--- Forgot_password.Designer.cs
+++ Forgot_password.Designer.cs
@@ -46,62 +46,69 @@ namespace Cafe_Management_Mini_Project
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(277, 28);
+            this.label1.Location = new System.Drawing.Point(208, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(191, 24);
+            this.label1.Size = new System.Drawing.Size(156, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Security Questions ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(184, 71);
+            this.label2.Location = new System.Drawing.Point(138, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(386, 17);
+            this.label2.Size = new System.Drawing.Size(285, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "(Answer all the Questions same as you filled in Registration)";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(169, 154);
+            this.label3.Location = new System.Drawing.Point(127, 125);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(151, 17);
+            this.label3.Size = new System.Drawing.Size(112, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Enter your Username :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(161, 220);
+            this.label4.Location = new System.Drawing.Point(121, 179);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(154, 17);
+            this.label4.Size = new System.Drawing.Size(115, 13);
             this.label4.TabIndex = 3;
             this.label4.Text = "Enter your Aadhar No :";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(62, 278);
+            this.label5.Location = new System.Drawing.Point(46, 226);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(253, 17);
+            this.label5.Size = new System.Drawing.Size(188, 13);
             this.label5.TabIndex = 4;
             this.label5.Text = "Enter your childhood bestfriend name :";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(169, 336);
+            this.label6.Location = new System.Drawing.Point(127, 273);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(146, 17);
+            this.label6.Size = new System.Drawing.Size(110, 13);
             this.label6.TabIndex = 5;
             this.label6.Text = "Enter your nickname :";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(340, 401);
+            this.button1.Location = new System.Drawing.Point(255, 326);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 6;
             this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
@@ -109,37 +116,41 @@ namespace Cafe_Management_Mini_Project
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(388, 154);
+            this.textBox1.Location = new System.Drawing.Point(291, 125);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(147, 22);
+            this.textBox1.Size = new System.Drawing.Size(111, 20);
             this.textBox1.TabIndex = 7;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(388, 215);
+            this.textBox2.Location = new System.Drawing.Point(291, 175);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(147, 22);
+            this.textBox2.Size = new System.Drawing.Size(111, 20);
             this.textBox2.TabIndex = 8;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(388, 273);
+            this.textBox3.Location = new System.Drawing.Point(291, 222);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(147, 22);
+            this.textBox3.Size = new System.Drawing.Size(111, 20);
             this.textBox3.TabIndex = 9;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(388, 331);
+            this.textBox4.Location = new System.Drawing.Point(291, 269);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(147, 22);
+            this.textBox4.Size = new System.Drawing.Size(111, 20);
             this.textBox4.TabIndex = 10;
             // 
             // Forgot_password
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -151,8 +162,10 @@ namespace Cafe_Management_Mini_Project
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Forgot_password";
             this.Text = "Forgot_password";
+            this.Load += new System.EventHandler(this.Forgot_password_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
